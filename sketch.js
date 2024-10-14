9b95c38957fb08f65e14c053bf2de0c606c6f332
Kyle <61264128+kylef7@users.noreply.github.com>
2024-10-14 12:10:28 -0400

    Added maximum gridsize & condensed/cleaned code

--- sketch.js
+++ sketch.js
@@ -1,34 +1,7 @@
 const grid = document.querySelector(".gridContainer");
 const btn = document.querySelector(".button");
 
-
-for (let i = 0; i < 256; i++) {
-    if (i % Math.sqrt(256) == 0) {
-
-        const newBreak = document.createElement("div");
-        newBreak.classList.add("break");
-        grid.append(newBreak);
-    }
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("divPixel");
-        grid.append(newDiv);
-
-        newDiv.addEventListener("mouseenter", darken);
-
-        function darken() {
-            newDiv.style.backgroundColor = "black";
-        }
-    }
-
-
-btn.addEventListener('click', askGrid);
-
-function askGrid() {
-    const gridSize = prompt("Pick the number of squares per side that you want!")
-    while (grid.firstChild) {
-        grid.removeChild(grid.lastChild);
-    }
-
+function gridCreate(gridSize) {
     for (let i = 0; i < gridSize * gridSize; i++) {
         if (i % Math.sqrt(gridSize * gridSize) == 0) {
             const newBreak = document.createElement("div");
@@ -46,6 +19,23 @@ function askGrid() {
             }
         
     }
+}
+
+gridCreate(16);
+
+btn.addEventListener('click', askGrid);
+
+function askGrid() {
+    let gridSize = prompt("Pick the number of squares per side that you want!")
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+
+    if (gridSize > 100) {
+        gridSize = 100;
+    }
+
+    gridCreate(gridSize);
 }
 
 
