f66e523dc84a0221db41f7daa75fc5048c6784f5
Kyle <61264128+kylef7@users.noreply.github.com>
2024-10-14 15:23:57 -0400

    Check if grid input is null or empty

--- sketch.js
+++ sketch.js
@@ -13,21 +13,23 @@ function gridCreate(gridSize) {
         grid.append(newDiv);
 
         let amount = 0;
+
         newDiv.addEventListener("mouseenter", darken);
-
-
 
         function darken() {
             amount = amount + 1;
 
             if (amount === 1) {
+
                 const valueOne = getRandomInt();
                 const valueTwo = getRandomInt();
                 const valueThree = getRandomInt();
 
                 newDiv.style.backgroundColor = `rgba(${valueOne}, ${valueTwo}, ${valueThree})`;
-                newDiv.style.opacity = "0.2";
             }
+
+            newDiv.style.opacity = "0.2";
+
 
             newDiv.style.opacity = amount / 10;
 
@@ -49,15 +51,18 @@ btn.addEventListener('click', askGrid);
 
 function askGrid() {
     let gridSize = prompt("Pick the number of squares per side that you want!")
-    while (grid.firstChild) {
-        grid.removeChild(grid.lastChild);
-    }
 
-    if (gridSize > 100) {
-        gridSize = 100;
-    }
+    if (gridSize != null && gridSize != "") {
+        while (grid.firstChild) {
+            grid.removeChild(grid.lastChild);
+        }
 
-    gridCreate(gridSize);
+        if (gridSize > 100) {
+            gridSize = 100;
+        }
+
+        gridCreate(gridSize);
+    }
 }
 
 
