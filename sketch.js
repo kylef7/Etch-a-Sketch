7e7547a03bf5ef99e982132ede3a126ac644244f
Kyle <61264128+kylef7@users.noreply.github.com>
2024-10-14 13:58:32 -0400

    Added randomized RGB values

--- sketch.js
+++ sketch.js
@@ -7,30 +7,39 @@ function gridCreate(gridSize) {
             const newBreak = document.createElement("div");
             newBreak.classList.add("break");
             grid.append(newBreak);
-        } 
-            const newDiv = document.createElement("div");
-            newDiv.classList.add("divPixel");
-            grid.append(newDiv);
+        }
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("divPixel");
+        grid.append(newDiv);
 
-            let amount = 0;
-            newDiv.addEventListener("mouseenter", darken);
+        let amount = 0;
+        newDiv.addEventListener("mouseenter", darken);
 
-            function darken() {
-                amount = amount + 1;
-                
-                if (amount === 1) {
-                newDiv.style.backgroundColor = "black";
+
+
+        function darken() {
+            amount = amount + 1;
+
+            if (amount === 1) {
+                const valueOne = getRandomInt();
+                const valueTwo = getRandomInt();
+                const valueThree = getRandomInt();
+
+                newDiv.style.backgroundColor = `rgba(${valueOne}, ${valueTwo}, ${valueThree})`;
                 newDiv.style.opacity = "0.2";
-                }
-
-                newDiv.style.opacity = amount / 10;
-
-                
-                newDiv.removeEventListener("mouseenter", darken);
-                newDiv.addEventListener("mouseenter", darken);
             }
-        
-            
+
+            newDiv.style.opacity = amount / 10;
+
+        }
+
+
+
+        function getRandomInt() {
+            return Math.floor(Math.random() * 255);
+        }
+
+
     }
 }
 
